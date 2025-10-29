770a2475d45e247f47e882ef1f7e0e255c4439ac
Jan Zakroczymski <jan.zakroczymski@gmail.com>
2025-10-29 23:11:13 +0100

    adding docker file for contenarization

--- lunar_lander/train.py
+++ lunar_lander/train.py
@@ -60,7 +60,7 @@ class DeepQNetwork(nn.Module):
         
         self.optimizer = optim.Adam(self.parameters(), lr=lr)
         self.loss = nn.MSELoss()
-        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.device = T.device('cuda' if T.cuda.is_available() else 'cpu')
         self.to(self.device)
 
     def forward(self, state):
